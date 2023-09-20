c67dd3fc88604441e44299cd28a5288ee19c9875
invis1b <86089227+invis1b@users.noreply.github.com>
2023-09-20 14:08:50 -0500

    Add files via upload

---
+++ mc_growth.py
@@ -0,0 +1,13 @@
+class Particle:
+    def __init__(self,index,lattice_indices,unitcell_index,hbnumber,hblist,distime):
+        self.index=index
+        self.lattice_indices=lattice_indices
+        self.unitcell_index=unitcell_index
+        self.hbnumber=hbnumber
+        self.hblist=hblist
+        self.distime=distime
+a=Particle_list
+def insert_particle():
+    
+
+    
